a912fd849d334224b0c578afce43fa23fd33817b
bnapky <napky33@gmail.com>
2021-03-30 01:28:39 -0600

    Postman collection added to samples, validate csv file is provided

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Param, Res, HttpStatus } from '@nestjs/common';
 import { VehicleService } from './services/vehicle/vehicle.service';
 import { UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
 import { UploadedFile } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('vehicles')
 export class VehicleController {
@@ -10,7 +11,19 @@ export class VehicleController {
 
   @Post(':provider')
   @UseInterceptors(FileInterceptor('vehicles'))
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Param('provider') provider: string) {
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('provider') provider: string,
+    @Res() res: Response
+  ) {
+
+    if (!file) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        success: false,
+        message: 'Vehicles CSV file is required.'
+      });
+    }
+
     const csv = file.buffer.toString();
 
     return this.vehicleService.importFromCsv(csv, provider);
